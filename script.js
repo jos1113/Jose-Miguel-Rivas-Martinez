8ec043e633cb4ece2a6ca49999d7129a7e91c320
jos1113 <jmiguel99.97@gmail.com>
2025-10-03 17:55:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function closeModal(id){
     document.getElementById(id).style.display = 'none';
 }
 
-// Cerrar modal si clic fuera del contenido
 window.onclick = function(event) {
     const modals = document.querySelectorAll('.modal');
     modals.forEach(modal => {
